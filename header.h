da6f965559aebf300e490d97d99cc6e435708555
Justin Kuhn <justinkuhn98@gmail.com>
2018-10-12 12:13:01 -0700

    Final

---
+++ header.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+
+using namespace std;
+
+struct StudentNode
+{
+    string name;
+    int age;
+    string major;
+    double gpa;
+    StudentNode *next;
+};
+
+void readFile(StudentNode *&templist);
+void display(StudentNode *templist);
+void findStudent(StudentNode *templist, string name);
+void remove(StudentNode *&templist);
+void updatedList(StudentNode *templist);
+void avgGPA(StudentNode *templist);
+
+
+#endif // HEADER_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
+/**********************************************************
+ * AUTHOR       : Justin Kuhn, Vincent Li, Brad Kim
+ * Lab       #10: Student List
+ * CLASS        : CS3A
+ * SECTION      : MW: 1:00p - 2:25p, TR: 1:00p - 3:05p
+ * Due Date     : 10/12/18
+*********************************************************/
+#include "header.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello Worwd!" << endl;
-    return 0;
+    StudentNode *head = NULL;
+    string name;
+
+
+    name = "Anna White";
+
+    readFile(head);
+
+    //Testing display Function
+    display(head);
+
+    //Testing findSTudent function;
+    findStudent(head, name);
+
+    //Testing remove first node function;
+    remove(head);
+
+    //Testing display new  Student list after node removal
+    updatedList(head);
+
+    //Testing display average GPA
+    avgGPA(head);
 }
